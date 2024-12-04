7efa952a8dcd7e377192764cb25de3bb54644826
Rohan Mallick <rohanmallick016@gmail.com>
2024-12-05 02:40:38 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8900'
-ENV = 'dev'
+SERVER_URL = 'https://calc-bee.vercel.app'
+PORT = '443'
+ENV = 'prod'
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
